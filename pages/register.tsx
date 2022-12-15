7572dc759eb48bc0df151742acf2d580b1641960
MuriloFuza <muriloacademix@gmail.com>
2022-12-15 11:02:36 -0400

    Activity: Register participant and show key - December 14, 2022

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,15 +1,31 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import { Button, Flex, FormLabel, Input, InputGroup, InputLeftElement, Link, NumberInput, NumberInputField, Text } from '@chakra-ui/react'
+import { Button, Flex, FormLabel, Input, InputGroup, InputLeftElement, Link, NumberInput, NumberInputField, Text, Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton, useDisclosure } from '@chakra-ui/react'
 import NextLink from 'next/link'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { AiOutlineIdcard, AiOutlineMail, AiOutlineUser } from 'react-icons/ai'
+import axios from 'axios'
 
 export default function Home() {
-  const parse = (val:string) =>{
-    return val.replace('.','').replace('-','')
-  }
+
   const[cpf,setcpf] = useState("") 
+  const[key,setKey] = useState() 
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [university, setUniversity] = useState()
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  useEffect(() => {
+    console.log(cpf)
+  }, [cpf])
+
   const format = (val:string) => {
     return val.replace(/\D/g, '') // substitui qualquer caracter que nao seja numero por nada
     .replace(/(\d{3})(\d)/, '$1.$2') // captura 2 grupos de numero o primeiro de 3 e o segundo de 1, apos capturar o primeiro grupo ele adiciona um ponto antes do segundo grupo de numero
@@ -17,6 +33,25 @@ export default function Home() {
     .replace(/(\d{3})(\d{1,2})/, '$1-$2')
     .replace(/(-\d{2})\d+?$/, '$1') // captura 2 numeros seguidos de um traço e não deixa ser digitado mais nada
   }
+
+  const cadastrar = () => {
+    axios.post('http://localhost:8001/user/create', {
+      name,
+      cpf,
+      email,
+      university
+    }).then((response) => {
+      console.log(response)
+      console.table(response.data)
+
+      if(!response.data.error){
+        onOpen()
+        setKey(response.data.key)
+      }
+
+    })
+  }
+
   return (
 <Flex  flexDir='column' alignItems='center'>
     <img  src='/engine.png' width='350px' height='230px' />
@@ -32,7 +67,7 @@ export default function Home() {
       >
       <AiOutlineUser color='gray.100' />
       </InputLeftElement>
-      <Input errorBorderColor='crimson' type='email' placeholder='Username' textColor={'#7C7C8A'}/>
+      <Input  onChange={(e) => setName(e.target.value)} value={name} errorBorderColor='crimson' type='email' placeholder='Username' textColor={'#7C7C8A'}/>
     </InputGroup>
 
       <FormLabel  width='sm' textAlign='left'  > E-mail</FormLabel>
@@ -40,16 +75,15 @@ export default function Home() {
       <InputLeftElement
         pointerEvents='none'
       ><AiOutlineMail color='gray.100' /></InputLeftElement>
-      <Input errorBorderColor='crimson' type='email' placeholder='gabriel@example.com' textColor={'#7C7C8A'}/>
+      <Input  onChange={(e) => setEmail(e.target.value)} value={email} errorBorderColor='crimson' type='email' placeholder='gabriel@example.com' textColor={'#7C7C8A'}/>
     </InputGroup>
 
     <FormLabel  width='sm' textAlign='left' > CPF</FormLabel>
 
+
     <InputGroup marginBottom={2} background='#202020' alignSelf='center' width='sm'>
       <InputLeftElement><AiOutlineIdcard color='gray.100' /></InputLeftElement>
-      <NumberInput  width='full' max={12} errorBorderColor='crimson' textColor={'#7C7C8A'} value={format(cpf)} onChange={(str)=>setcpf(parse(str))} >
-        <NumberInputField placeholder='123.456.789-90' pl={10}/>
-      </NumberInput>
+      <Input  width='full' maxLength={14} errorBorderColor='crimson' textColor={'#7C7C8A'}  value={format(cpf)} onChange={(e)=>setcpf(e.target.value)}/>
     </InputGroup>
 
     <FormLabel  width='sm' textAlign='left'  > Faculdade</FormLabel>
@@ -57,14 +91,34 @@ export default function Home() {
       <InputLeftElement
         pointerEvents='none'
       ><AiOutlineIdcard color='gray.100' /></InputLeftElement>
-      <Input errorBorderColor='crimson' type='email' placeholder='UNIR' textColor={'#7C7C8A'}/>
+      <Input  onChange={(e) => setUniversity(e.target.value)} value={university} errorBorderColor='crimson' type='email' placeholder='UNIR' textColor={'#7C7C8A'}/>
     </InputGroup>
 
-  <Button marginTop={4} alignSelf='center' width='280px' textColor={'black'} colorScheme={'twitter'} variant='solid' onClick={()=> {
-        console.log("click")
-      }}>Cadastrar</Button>
+  <Button onClick={cadastrar} marginTop={4} alignSelf='center' width='280px' textColor={'black'} colorScheme={'twitter'} variant='solid'
+    >Cadastrar</Button>
 
   <Link marginTop={4} color='gray.400' alignSelf='center' as={NextLink} href='/'>Cancelar</Link>
+
+
+  <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textColor={'black'}>Cadastro Realizado</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody textColor={'black'}>
+              Seu cadastro foi realizado com sucesso!
+              A chave de acesso lhe será mostrada apenas está vez, guarde bem pois a mesma
+              será necessária para se cadastrar nos eventos.<br/>
+              Chave: {key}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='twitter' mr={3} onClick={onClose} as={NextLink} href='/event_list'>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+  </Modal>
 
 
   </Flex> 
